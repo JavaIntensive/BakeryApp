91c1be7e80ccb8d03c760774f098ae6f8fd84474
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-05 13:23:57 -0500

    practice .ignore

--- src/ProductDB.java
+++ src/ProductDB.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class ProductDB {
+    private ArrayList<Product> dbList;
+    public ProductDB()
+    {
+        dbList.add(new Product("name","price","rest","exp","quantity"));
+        dbList.add(new Product("Ham","j","","",1);
+        dbList.add(new Product("","","","",2);
+        dbList.add(new Product("","","","",3);
+        dbList.add(new Product("","","","",4);
+        dbList.add(new Product("","","","",5);
+        dbList.add(new Product("","","","",6);
+        dbList.add(new Product("","","","",7);
+        dbList.add(new Product("","","","",8);
+        dbList.add(new Product("","","","",9);
+        dbList.add(new Product("","","","",10);
+        dbList.add(new Product("","","","",11);
+        dbList.add(new Product("","","","",12);
+        dbList.add(new Product("","","","",13);
+        dbList.add(new Product("","","","",14);
+        dbList.add(new Product("","","","",15);
+        
+        
+        
+    }
 }
