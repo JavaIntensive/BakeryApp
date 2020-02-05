2f8e1777b7df783411d015de356062641f342924
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-05 13:17:13 -0500

    adding product app

--- src/ProductApp.java
+++ src/ProductApp.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class ProductApp {
+    public static void main(String[] args) {
+        // ArrayList or prodcut
+        // Search method
+        ArrayList<Product> product;
+
+        // search staff
+        //1. accept input from user
+        //2.
+
+
+    }
 }
